55b4e8e08f77409d4bf18c57ae4470312c891c9e
aminacherop <cheropamina26@gmail.com>
2025-05-08 23:16:01 +0300

    comprehension and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
+    print(return_evens([1,2,3,4,5,6]))
     pass
 
 def make_exclamation(sentence_list):
+    return [sentence + "!" for sentence in sentence_list]
     pass
